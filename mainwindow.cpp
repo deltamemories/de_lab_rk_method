b5524e7898acd85036587fc92bac2c28de01d5fc
deltamemories <maxenot123@gmail.com>
2026-05-05 08:02:07 +0200

    feat(gui): add exception handling

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 
 #include "mainwindow.h"
 #include "ui_MainWindow.h"
+#include <QMessageBox>
 
 
 MainWindow::MainWindow(QWidget *parent) : QWidget(parent), ui(new Ui::MainWindow) {
@@ -57,43 +58,48 @@ void MainWindow::setupTable() {
 }
 
 void MainWindow::on_solveButton_clicked() {
-    double x0 = ui->x0Edit->text().toDouble();
-    double y0 = ui->y0Edit->text().toDouble();
-    double xEnd = ui->xEndEdit->text().toDouble();
-    double h = ui->hEdit->text().toDouble();
-    double eps = ui->epsEdit->text().toDouble();
+    try {
+        double x0 = ui->x0Edit->text().toDouble();
+        double y0 = ui->y0Edit->text().toDouble();
+        double xEnd = ui->xEndEdit->text().toDouble();
+        double h = ui->hEdit->text().toDouble();
+        double eps = ui->epsEdit->text().toDouble();
 
-    Solver solver;
-    std::vector<PairedPoints> data = solver.solverWithDynamicStepPaired(x0, y0, xEnd, h, eps);
+        Solver solver;
+        std::vector<PairedPoints> data = solver.solverWithDynamicStepPaired(x0, y0, xEnd, h, eps);
 
-    QVector<double> x, yRK, yRef;
-    for (const auto& p : data) {
-        x.push_back(p.x);
-        yRK.push_back(p.y);
-        yRef.push_back(p.yRef);
+        QVector<double> x, yRK, yRef;
+        for (const auto& p : data) {
+            x.push_back(p.x);
+            yRK.push_back(p.y);
+            yRef.push_back(p.yRef);
+        }
+
+        ui->plot->graph(0)->setData(x, yRK);
+        // ui->plot->graph(1)->setData(x, yRef);
+
+        ui->plot->rescaleAxes();
+        if (ui->plot->xAxis->range().lower < 0) {
+            ui->plot->xAxis->setRangeLower(0);
+        }
+        ui->plot->replot();
+
+        ui->tableWidget->setRowCount(0);
+        for (int i = 0; i < data.size(); ++i) {
+            const auto& p = data[i];
+            double error = std::abs(p.y - p.yRef);
+
+            ui->tableWidget->insertRow(i);
+            ui->tableWidget->setItem(i, 0, new QTableWidgetItem(QString::number(p.x, 'g', 6)));
+            ui->tableWidget->setItem(i, 1, new QTableWidgetItem(QString::number(p.y, 'g', 6)));
+            ui->tableWidget->setItem(i, 2, new QTableWidgetItem(QString::number(p.yRef, 'g', 6)));
+            ui->tableWidget->setItem(i, 3, new QTableWidgetItem(QString::number(error, 'e', 3)));
+        }
+    } catch (const std::logic_error& e) {
+        QMessageBox::critical(this, "Ошибка расчета", e.what());
+    } catch (...) {
+        QMessageBox::warning(this, "Внимание", "Произошла неизвестная ошибка при расчете.");
     }
-
-    ui->plot->graph(0)->setData(x, yRK);
-    // ui->plot->graph(1)->setData(x, yRef);
-
-    ui->plot->rescaleAxes();
-    if (ui->plot->xAxis->range().lower < 0) {
-        ui->plot->xAxis->setRangeLower(0);
-    }
-    ui->plot->replot();
-
-    ui->tableWidget->setRowCount(0);
-    for (int i = 0; i < data.size(); ++i) {
-        const auto& p = data[i];
-        double error = std::abs(p.y - p.yRef);
-
-        ui->tableWidget->insertRow(i);
-        ui->tableWidget->setItem(i, 0, new QTableWidgetItem(QString::number(p.x, 'g', 6)));
-        ui->tableWidget->setItem(i, 1, new QTableWidgetItem(QString::number(p.y, 'g', 6)));
-        ui->tableWidget->setItem(i, 2, new QTableWidgetItem(QString::number(p.yRef, 'g', 6)));
-        ui->tableWidget->setItem(i, 3, new QTableWidgetItem(QString::number(error, 'e', 3)));
-    }
-
 }
 
 void MainWindow::on_refButton_clicked() {
